9240b12b2ba50336bff213fada62426e940bf127
Ayda Gonzalez <aydagonzalez15@gmail.com>
2024-02-26 15:23:00 -0500

    added button disabled

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ const wordDiv = document.querySelector(".flex-container");
 const columnEls = [document.querySelectorAll('.column')]
 const makeKeyboard = document.querySelector(".middle-section")
 const button =document.createElement('button');
-button.class="keyboard-button"
+
 
 
                                   /*----- event listeners -----*/
@@ -66,7 +66,8 @@ function renderSplitWordGenerator() {
 
 function renderWordDiv(){
   const word = splitWordGenerator;
-  wordDiv.innerHTML = word.map(() => `<div class="flex-item column"></div>`).join("");
+
+  // wordDiv.innerHTML = word.map(() => `<div class="flex-item column"></div>`).join("");
   console.log('WORD',word)
 }
 
@@ -90,7 +91,7 @@ function renderWord(button, letter){
     ALPH_LOOKUP.forEach(letter => {
     for (let i=0; i<1; i++) {
       const button = document.createElement('button');
-      button.class= letter
+      button.classList.add(letter)
       button.innerText = letter
       makeKeyboard.appendChild(button);
       button.addEventListener('click', evt => handleClick(evt.target, letter))
@@ -98,23 +99,6 @@ function renderWord(button, letter){
   }
   )
 }
-  
-
-function handleClick() {
-  if (splitWordGenerator.includes(letter)) {
-    button.style.backgroundColor = 'green';
-    correctGuess()
-    console.log (letter)
-
-
-
-  } else {
-    button.style.backgroundColor = 'grey';
-    button.style.opacity= '0.7';
-    wrongGuess()
-    console.log('NO');
-  }
-}
 
 
 
@@ -123,32 +107,14 @@ function handleClick(button, letter) {
     button.style.backgroundColor = 'green';
     correctGuess()
     console.log (letter)
-
   } else {
+    button.disabled = true
     button.style.backgroundColor = 'grey';
     button.style.opacity= '0.7';
     wrongGuess()
     console.log('NO');
   }
 }
-
-
-function handleClick(button, letter) {
-  if (splitWordGenerator.includes(letter)) {
-    button.style.backgroundColor = 'green';
-    correctGuess()
-    console.log (letter)
-
-  } else {
-    button.style.backgroundColor = 'grey';
-    button.style.opacity= '0.7';
-    wrongGuess()
-    console.log('NO');
-  }
-}
-
-
-
 
 
   
@@ -181,7 +147,6 @@ function wrongGuess() {
 
 
 
-
 function renderResults() {
   for (let key in results) { 
     pGuessEl= document.getElementById(`${key}-guess`);
@@ -189,6 +154,3 @@ function renderResults() {
 
   }
 }
-
-  
-
