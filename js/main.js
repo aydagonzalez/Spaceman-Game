155427ed752cd703245543bd0718ad0b67e6c029
Ayda Gonzalez <aydagonzalez15@gmail.com>
2024-02-26 09:11:19 -0500

    updates to js scores update

--- js/main.js
+++ js/main.js
@@ -1,111 +1,102 @@
 console.log( "ARE YOU THERE AYDA?")
-  /*----- constants -----*/
-    const WORDS = ['wavelength', 'science', 'hypodermic', 'software', 'engineer', 'homework'] 
-    // -words to randomize with (fisher yates shuffle -- will shuffle array)
-    const ALPH_LOOKUP = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',];
-    
-
-   const guessLimit = 5 //this will set # of total guess limit
-
-// **Ayda: look up how to accept user input in the browser**//
-// -alphabet as an array -> use forEach method
+                                  /*----- constants -----*/
+  const WORDS = ['wavelength', 'science', 'hypodermic', 'software', 'engineer', 'homework'] 
+  const guessLimit = 5 //this will set # of total guess limit
 
 
-  /*----- state variables -----*/
-let board;
-let board2;
-let maxWrong = 5
-//   cGuess: 0, // # total correct player guesses count
-//   wGuess: 0, // # total incorrect player guesses count
-let results;
+
+                                  /*----- state variables -----*/
+let maxWrongLimit = 5;
+let wrongGuessRem = maxWrongLimit;
+
+let results ={   
+cG: 0, // # total correct player guesses count
+wG: 0, // # total incorrect player guesses count}
+}
 let wordCount;//  = {} // how many letters left to guess 
 let winner; 
-let splitWordGenerator;
-  /*----- cached elements  -----*/
+
+                                  /*----- cached elements  -----*/
+
+
+  const makeKeyboard = document.querySelector(".middle-section")
   const wordDiv = document.querySelector(".flex-container");
-  const columnEls = [...document.querySelectorAll('.column')]
+  const columnEls = [document.querySelectorAll('.column')]
+
   console.log ('TRIAL:', columnEls)
 //listen for clicks:
 
-    // -when player clicks for random word
+    // √ -when player clicks for random word
     // -Word display
-    // -keyboard display
+    // √ -keyboard display
     // -spaceman image
     // -when player guesses a letter
     // -when player whats to clear the Game or play again
 
 
-  /*----- event listeners -----*/
+                                  /*----- event listeners -----*/
+//  Button: event listener for clicking the letters guessed,
+// when players want to start over 
+// √ when players click a letter
 
-//   document.querySelector('.start-button')
-//   .addEventListener('click', handleStartButton);
-
-  // const buttons = document.querySelectorAll('.buttons')
-  // let textarea = buttons.innerText
-
-  // let text = buttons.innerText
-  // console.log(text)
-  // buttons.forEach(btn => {
-  //   btn.addEventListener('click', () => {
-  //     textarea
-  //   })
-  // })
-
+/// fronm geeks to geeks
 function getKeyboard() {
-  let buttons = 'abcdefghijklmnopqrstuvwxyz'
-  let splitButtons = buttons.split('').map(letter =>
-    `
-    <button id="` + letter + ` onClick="handleGuess('`+ letter + `')">` + letter + `</button> `).join('');
-
-   document.getElementById('keyboard').innerHTML =splitButtons ;
+  for (let i = 97; i <=122; i++) {
+    const button =document.createElement('button');
+    button.innerText = String.fromCharCode(i);
+    makeKeyboard.appendChild(button);
+    button.addEventListener('click', evt =>  handleClick(evt.target, String.fromCharCode(i)))
+  }
 }
 
-getKeyboard();
 
-functionha
 
-document.getElementById()
 
-//   // const textArea = kButton.addEventListener('click', handlekButton);
+
+function renderGuesses() {
+  for (let key in guesses) { 
+    pGuessEl= document.getElementById(`${key}-guess`);
+      pGuessEl.innerHTML = `${guesses[key]}`;
+  }
+}
+
+
+
   
-//   kButton.forEach(btn => {
-//   btn.addEventListener('click', () => {
-//     textArea.value += btn.innerText
-//   })
-// })
-  // function handlekButton(){
-  //   kButton.innerText = 'a'
-  // }
-  // handlekButton()
+  function handleClick(button, letter) {
+
+    if (splitWordGenerator.includes(letter)) {
+      results.cG++
+      button.style.backgroundColor = 'red';
+      // columnEls.querySelectorAll('columns').innerText =letter
+      document.createElement('button').innerHTML = letter
+      console.log(letter);
+      renderResults();
+    
+  
+      // columnEls.style.backgroundColor ='black';
+    } else {
+      wrongGuessRem--
+      console.log('NO');
+            // results.cG =+ 1;
+      wordDiv.style.backgroundColor ='red';
+      wordDiv.style.backgroundColor ='white';
 
 
-  // function handleChoice(evt){
-  //   //need a Guard (do nothing unless one of the three buttons were clicked)
-  //     if (evt.target.tagName !== "BUTTON") return;
-  //     // console.log(evt.target.tagName)
-  //     //if player made c hoice
-  //     results.p = evt.target.innerText.toLowerCase();
-  //       //compute random choice for computer
-  //     results.c = getRandomRPS();
-  //     winner = getWinner();
-  //     scores[winner] += 1;
-  //     render();
-  //   }
+     }
+}
 
 
 
-//  -Button: event listener for clicking the letters guessed,
-// when players want to start over 
-// when players click a letter
 
-  /*----- functions -----*/
+                                 /*----- functions -----*/
 init();
 
 //   √ will initialize all states, => calls render();
 
   // √ random word function getRandomWord()
   // √ display the length of the word for the player
-  // link keyboard to letters
+  // √ link keyboard to letters
   // Guessed letters are shown
   // display the number of chances remaining
   // keep track of number of wrong guesses
@@ -115,27 +106,12 @@ init();
   // GOOD TO GO **//
 function init() {
 
-  board = 
-    ['c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9', 'c10']
-    board2 = 
-    {c1: '', c2: '', c3: '', c4: '', c5: '', c6: '', c7: '', c8: '', c9: '', c10: ''
-    }
-  guesses = {
-    cG: 2, //player guesses
-    wG: 0 // wrong guesses
-  };
-  results ={
-    cG: '', //player guesses
-    wG: ''
-  };
 
   winner = {
-    //THIS IS EMPTY--LOOKOVER**//
   };
 
   splitWordGenerator = renderSplitWordGenerator()
 
-  
 
   render();
 }
@@ -143,21 +119,15 @@ function init() {
 function renderWord(){
   const word = splitWordGenerator;
   wordDiv.innerHTML = word.map(() => `<div class="flex-item column"></div>`).join("");
+  console.log(word)
 }
 
-
-function handleStartButton(evt){
-  //need a Guard (do nothing unless one of the three buttons were clicked)
-    if (evt.target.tagName !== "start-button") return;
-    // console.log(evt.target.tagName);
-    render();
-  }
-
 //GOOD TO GO **//
+
 function renderGuesses() {
-  for (let key in guesses) { 
+  for (let key in results) { 
     pGuessEl= document.getElementById(`${key}-guess`);
-      pGuessEl.innerHTML = `${guesses[key]}`;
+      pGuessEl.innerHTML = `${results[key]}`;
   }
 }
 
@@ -167,6 +137,7 @@ function renderResults() {
 
   // GOOD TO GO-ISH**//
 function render() {
+  getKeyboard();
   renderGuesses();
   renderResults();
   renderSplitWordGenerator();
@@ -213,30 +184,3 @@ function columnLoop2() {
   
 
   
-
-
-
-
-
-const REAL= {
-  words : splitWordGenerator,
-  board3 : ['c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9', 'c10'],
-  eachLetter: '',
-
-
-
- 
-  init: function(newWordonDiv) {
-    this.wordOnDiv = newWordonDiv
-  },
-
-  loopWord: function(word) {
-    this.words.forEach(word => {
-      return word
-    });
-    this.wordOnDiv(word, this.words);
-    }
-
-};
-
-
